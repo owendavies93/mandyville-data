479419d077f6aa002028aa0d47a913ca9018eb8a
Owen Davies <owen@odavi.es>
2021-03-16 22:06:14 +0000

    Add Mandyville::Teams

---
+++ lib/Mandyville/Teams.pm
@@ -0,0 +1,172 @@
+package Mandyville::Teams;
+
+use Mojo::Base -base, -signatures;
+
+use Mandyville::Database;
+use Mandyville::Countries;
+
+use SQL::Abstract::More;
+
+=head1 NAME
+
+  Mandyville::Teams - fetch and store team data
+
+=head1 SYNOPSIS
+
+  use Mandyville::Teams;
+  my $dbh  = Mandyville::Database->new->rw_db_handle();
+  my $sqla = SQL::Abstract::More->new;
+  my $teams = Mandyville::Teams->new({
+      dbh  => $dbh,
+      sqla => $sqla,
+  });
+
+=head1 DESCRIPTION
+
+  This module provides methods for fetching and storing team
+  data. It primarily uses match data from the football-data
+  API to achieve this.
+
+=head1 METHODS
+
+=over
+
+=item dbh 
+
+  A read-write handle to the Mandyville database.
+
+=item sqla
+
+  An instance of SQL::Abstract::More.
+
+=cut
+
+has 'dbh'  => sub { shift->{dbh} };
+has 'sqla' => sub { shift->{sqla} };
+
+=item new ([ OPTIONS ])
+
+  Creates a new instance of the module, and sets the various required
+  attributes. C<OPTIONS> is a hashref that can contain the following
+  fields:
+
+    * dbh  => A read-write handle to the Mandyville database
+    * sqla => An instance of SQL::Abstract::More
+
+  If these options aren't passed in, they will be instantied by this
+  method. However, it's recommended to pass these options in for 
+  performance and memory usage reasons.
+
+=cut
+
+sub new($class, $options) {
+    $options->{dbh}  //= Mandyville::Database->new->rw_db_handle();
+    $options->{sqla} //= SQL::Abstract::More->new;
+
+    my $self = {
+        dbh       => $options->{dbh},
+        sqla      => $options->{sqla},
+    };
+
+    bless $self, $class;
+    return $self;
+}
+
+=item get_or_insert ( NAME, COUNTRY_ID, FOOTBAL_DATA_ID ) 
+
+  Fetch the team assoicated with the C<NAME>, C<COUNTRY_ID> and 
+  C<FOOTBAL_DATA_ID>. If the team isn't found, the team is inserted
+  into the database. Returns a hashref of the team data that was either
+  fetched or inserted, with the C<name>, C<id>, C<country_id> and
+  C<football_data_id> attributes.
+
+=cut
+
+sub get_or_insert($self, $name, $country_id, $football_data_id) {
+    my ($stmt, @bind) = $self->sqla->select(
+        -columns => 'id',
+        -from    => 'teams',
+        -where   => {
+            'country_id'       => $country_id,
+            'football_data_id' => $football_data_id,
+            'name'             => $name,
+        }
+    );
+
+    my ($id) = $self->dbh->selectrow_array($stmt, undef, @bind);
+
+    if (!defined $id) {
+        ($stmt, @bind) = $self->sqla->insert(
+            -into      => 'teams',
+            -values    => {
+                'country_id'       => $country_id,
+                'football_data_id' => $football_data_id,
+                'name'             => $name,
+            },
+            -returning => 'id',
+        );
+
+        ($id) = $self->dbh->selectrow_array($stmt, undef, @bind);
+    }
+
+    return {
+        id               => $id,
+        country_id       => $country_id,
+        name             => $name,
+        football_data_id => $football_data_id,
+    };
+}
+
+=item get_or_insert_team_comp ( TEAM_ID, SEASON, COMP_ID )
+
+  Fetch the team's season assoicated with the given C<TEAM_ID>,
+  C<SEASON> and C<COMP_ID>. C<COMP_ID> is the mandyville database
+  competition ID, C<TEAM_ID> is the mandyville database team ID,
+  C<SEASON> is the starting year of the season. If the team's season
+  isn't found, it's inserted into the database. Returns a hashref of
+  the team's season data that was either fetched or inserted, wth the 
+  C<id>, C<team_id>, C<season> and C<competition_id> attributes.
+
+=cut
+
+sub get_or_insert_team_comp($self, $team_id, $season, $comp_id) {
+    my ($stmt, @bind) = $self->sqla->select(
+        -columns => 'id',
+        -from    => 'teams_competitions',
+        -where   => {
+            'competition_id' => $comp_id,
+            'season'         => $season,
+            'team_id'        => $team_id,
+        }
+    );
+
+    my ($id) = $self->dbh->selectrow_array($stmt, undef, @bind);
+
+    if (!defined $id) {
+        ($stmt, @bind) = $self->sqla->insert(
+            -into   => 'teams_competitions',
+            -values => {
+                'competition_id' => $comp_id,
+                'season'         => $season,
+                'team_id'        => $team_id,
+            },
+            -returning => 'id',
+        );
+
+        ($id) = $self->dbh->selectrow_array($stmt, undef, @bind);
+    }
+
+    return {
+        id      => $id,
+        team_id => $team_id,
+        season  => $season,
+        comp_id => $comp_id,
+    };
+}
+
+=back
+
+=cut
+
+1;
+
---
+++ t/Mandyville/Teams.t
@@ -0,0 +1,105 @@
+#!/usr/bin/env perl
+
+use Mojo::Base -strict;
+
+use Mandyville::Competitions;
+use Mandyville::Countries;
+use Mandyville::Database;
+
+use Test::Exception;
+use Test::More;
+
+######
+# TEST use/require
+######
+
+use_ok 'Mandyville::Teams';
+require_ok 'Mandyville::Teams';
+
+use Mandyville::Teams;
+
+######
+# TEST get_or_insert
+######
+
+{
+    my $db = Mandyville::Database->new;
+    my $countries = Mandyville::Countries->new({
+        dbh => $db->rw_db_handle(),
+    });
+
+    my $teams = Mandyville::Teams->new({
+        dbh => $db->rw_db_handle(),
+    });
+
+    my $football_data_id = 100;
+    my $name = 'Chelsea';
+    my $country_id = $countries->get_country_id('England');
+
+    dies_ok { $teams->get_or_insert(); } 'get_or_insert: dies without args';
+
+    my $data = $teams->get_or_insert($name, $country_id, $football_data_id);
+    my $id   = $data->{id};
+
+    ok( $id, 'get_or_insert: id returned after insert' );
+
+    cmp_ok( $data->{name}, 'eq', $name, 'get_or_insert: name matches' );
+
+    $data = $teams->get_or_insert($name, $country_id, $football_data_id);
+
+    cmp_ok( $data->{id}, '==', $id, 'get_or_insert: id matches after get' );
+}
+
+######
+# TEST get_or_insert_team_comp
+######
+
+{
+    my $db = Mandyville::Database->new;
+    my $countries = Mandyville::Countries->new({
+        dbh => $db->rw_db_handle(),
+    });
+
+    my $teams = Mandyville::Teams->new({
+        dbh => $db->rw_db_handle(),
+    });
+
+    my $comps = Mandyville::Competitions->new({
+        dbh => $db->rw_db_handle(),
+    });
+    
+    my $football_data_id = 100;
+    my $name = 'Chelsea';
+    my $country_id = $countries->get_country_id('England');
+    my $season = '2020';
+    my $comp_name = 'Premier League';
+    my $comp_id = '2000';
+
+    my $comp    = $comps->get_or_insert($comp_name, $country_id, $comp_id, 1);
+    my $team    = $teams->get_or_insert($name, $country_id, $football_data_id);
+    my $team_id = $team->{id};
+
+    dies_ok { $teams->get_or_insert_team_comp() }
+              'get_or_insert_team_comp: dies without args';
+
+    my $team_comp = $teams->get_or_insert_team_comp(
+        $team_id, $season, $comp->{id}
+    );
+
+    ok( $team_comp, 'get_or_insert_team_comp: inserts successfully' );
+
+    cmp_ok( $team_comp->{team_id}, '==', $team_id,
+            'get_or_insert_team_comp: team IDs match' );
+
+    my $team_comp_id = $team_comp->{id};
+
+    $team_comp = $teams->get_or_insert_team_comp(
+        $team_id, $season, $comp->{id}
+    );
+
+    cmp_ok( $team_comp->{id}, '==', $team_comp_id,
+            'get_or_insert_team_comp: team comp ID matches after get' );
+}
+
+done_testing();
+
