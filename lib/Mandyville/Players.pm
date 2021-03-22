7c656332ba6c78042b230c709b966365b9f46914
Owen Davies <owen@odavi.es>
2021-03-22 07:20:58 +0000

    Add update_fixture_info to Mandyville::Players

--- lib/Mandyville/Players.pm
+++ lib/Mandyville/Players.pm
@@ -18,8 +18,25 @@ use SQL::Abstract::More;
   my $dbh  = Mandyville::Database->new->rw_db_handle();
   my $sqla = SQL::Abstract::More->new;
 
+  my $teams = Mandyville::Teams->new({
+      dbh  => $dbh,
+      sqla => $sqla,
+  });
+
+  my $comps = Mandyville::Competitions->new({});
+
+  my $fixtures = Mandyville::Fixtures->new({
+      comps => $comps,
+      dbh   => $dbh,
+      sqla  => $sqla,
+      teams => $teams,
+  });
+
   my $players = Mandyville::Players->new({
+      api       => Mandyville::API::FootballData->new,
+      comps     => $comps,
       countries => Mandyville::Countries->new,
+      fixtures  => $fixtures,
       dbh       => $dbh,
       sqla      => $sqla,
   });
@@ -35,23 +52,43 @@ use SQL::Abstract::More;
 
 =over
 
+=item api
+
+  An instance of Mandyville::API::FootballData
+
+=item comps
+
+  An instance of Mandyville::Competitions.
+
 =item countries
 
   An instance of Mandyville::Countries.
 
-=item dbh 
+=item dbh
 
   A read-write handle to the Mandyville database.
+
+=item fixtures
+
+  An instance of Mandyville::Fixtures.
 
 =item sqla
 
   An instance of SQL::Abstract::More.
 
+=item teams
+
+  An instance of Mandyville::Teams.
+
 =cut
 
+has 'api'       => sub { shift->{api} };
+has 'comps'     => sub { shift->{comps} };
 has 'countries' => sub { shift->{countries} };
 has 'dbh'       => sub { shift->{dbh} };
+has 'fixtures'  => sub { shift->{fixtures} };
 has 'sqla'      => sub { shift->{sqla} };
+has 'teams'     => sub { shift->{teams} };
 
 =item new ([ OPTIONS ])
 
@@ -63,12 +100,13 @@ has 'sqla'      => sub { shift->{sqla} };
     * sqla => An instance of SQL::Abstract::More
 
   If these options aren't passed in, they will be instantied by this
-  method. However, it's recommended to pass these options in for 
+  method. However, it's recommended to pass these options in for
   performance and memory usage reasons.
 
 =cut
 
 sub new($class, $options) {
+    $options->{api}  //= Mandyville::API::FootballData->new;
     $options->{dbh}  //= Mandyville::Database->new->rw_db_handle();
     $options->{sqla} //= SQL::Abstract::More->new;
 
@@ -77,14 +115,60 @@ sub new($class, $options) {
         sqla => $options->{sqla},
     });
 
-    my $self = {
+    $options->{comps} //= Mandyville::Competitions->new({
+        api       => $options->{api},
         countries => $options->{countries},
         dbh       => $options->{dbh},
         sqla      => $options->{sqla},
+    });
+
+    $options->{teams} //= Mandyville::Teams->new({
+        dbh  => $options->{dbh},
+        sqla => $options->{sqla},
+    });
+
+    $options->{fixtures} //=Mandyville::Fixtures->new({
+        comps => $options->{comps},
+        dbh   => $options->{dbh},
+        sqla  => $options->{sqla},
+        teams => $options->{teams},
+    });
+
+    my $self = {
+        api       => $options->{api},
+        comps     => $options->{comps},
+        countries => $options->{countries},
+        dbh       => $options->{dbh},
+        fixtures  => $options->{fixtures},
+        sqla      => $options->{sqla},
+        teams     => $options->{teams},
     };
 
     bless $self, $class;
     return $self;
+}
+
+=item get_by_football_data_id ( FOOTBALL_DATA_ID )
+
+  Fetch the player associated with the given C<FOOTBALL_DATA_ID>. Does
+  no insertion into the database; returns undef if no player is found,
+  returns the mandyville database ID of the found player if a player is
+  found.
+
+=cut
+
+sub get_by_football_data_id($self, $football_data_id) {
+    my ($stmt, @bind) = $self->sqla->select(
+        -columns => 'id',
+        -from    => 'players',
+        -where   => {
+            football_data_id => $football_data_id,
+        },
+    );
+
+    my ($id) = $self->dbh->selectrow_array($stmt, undef, @bind);
+
+    return $id;
 }
 
 =item get_or_insert ( FOOTBALL_DATA_ID, PLAYER_INFO )
@@ -122,7 +206,7 @@ sub get_or_insert($self, $football_data_id, $player_info) {
         my $country_id =
             $self->countries->get_country_id($player_info->{country_name});
 
-        die "No country with name $country_name found"
+        die 'No country with name' . $player_info->{country_name} . 'found'
             unless defined $country_id;
 
         ($stmt, @bind) = $self->sqla->insert(
@@ -145,6 +229,126 @@ sub get_or_insert($self, $football_data_id, $player_info) {
         last_name    => $player_info->{last_name},
         country_name => $player_info->{country_name},
     };
+}
+
+=item update_fixture_info ( FIXTURE_DATA )
+
+  Process the player data for a fixture, inserting player data where
+  necessary. The C<FIXTURE_DATA> paramater should be hashref in the
+  same format as the JSON shown in
+  football-data.org/documentation/api#match.
+
+  Calls out to the football-data API to fetch player info if the
+  player isn't previously known.
+
+=cut
+
+sub update_fixture_info($self, $fixture_data) {
+    my $fixture_info = $self->fixtures->process_fixture_data($fixture_data);
+    my $fixture_id   = $fixture_info->{id};
+
+    my $home_id = $fixture_info->{home_team_id};
+    $self->_process_team_info($fixture_id, $home_id, $fixture_data);
+
+    my $away_id = $fixture_info->{away_team_id};
+    return $self->_process_team_info($fixture_id, $away_id, $fixture_data);
+}
+
+sub _process_team_info($self, $fixture_id, $team_id, $fixture_data) {
+    my $team_info = $fixture_data->{homeTeam}->{id} == $team_id ?
+                    $fixture_data->{homeTeam} : $fixture_data->{awayTeam};
+
+    my $starters = $team_info->{lineup};
+    my $subs     = $team_info->{bench};
+
+    my %bookings = map {
+        $_->{player}->{id} => $_->{card}
+    } @{$fixture_data->{bookings}};
+
+    my %subsOff = map {
+        $_->{playerOut}->{id} => $_->{minute}
+    } @{$fixture_data->{substitutions}};
+
+    my %subsOn = map {
+        $_->{playerIn}->{id} => $_->{minute}
+    } @{$fixture_data->{substitutions}};
+
+    # TODO: reduce duplication
+    foreach my $player (@$starters) {
+        my $player_id = $self->get_by_football_data_id($player->{id});
+
+        $player_id = $self->_get_api_info_and_store($player->{id})->{id}
+            if !defined $player_id;
+
+        my $yellow = $self->_has_card($player->{id}, \%bookings, 'YELLOW');
+        my $red    = $self->_has_card($player->{id}, \%bookings, 'RED');
+
+        my $minutes_played = exists $subsOff{$player->{id}} ?
+                             $subsOff{$player->{id}} : 90;
+
+        my $info = {
+            player_id   => $player_id,
+            fixture_id  => $fixture_id,
+            team_id     => $team_id,
+            minutes     => $minutes_played,
+            yellow_card => $yellow || 0,
+            red_card    => $red || 0,
+        };
+
+        $self->_insert_player_fixture($info);
+    }
+
+    foreach my $player (@$subs) {
+        my $player_id = $self->get_by_football_data_id($player->{id});
+
+        $player_id = $self->_get_api_info_and_store($player->{id})->{id}
+            if !defined $player_id;
+
+        my $yellow = $self->_has_card($player->{id}, \%bookings, 'YELLOW');
+        my $red    = $self->_has_card($player->{id}, \%bookings, 'RED');
+
+        my $minutes_played = exists $subsOn{$player->{id}} ?
+                             90 - $subsOn{$player->{id}} : 0;
+
+        my $info = {
+            player_id   => $player_id,
+            fixture_id  => $fixture_id,
+            team_id     => $team_id,
+            minutes     => $minutes_played,
+            yellow_card => $yellow || 0,
+            red_card    => $red || 0,
+        };
+
+        $self->_insert_player_fixture($info);
+    }
+
+    return 1;
+}
+
+sub _get_api_info_and_store($self, $player_id) {
+    my $player_info = $self->api->player($player_id);
+    my $to_insert = {
+        first_name   => $player_info->{firstName},
+        last_name    => $player_info->{lastName},
+        country_name => $player_info->{nationality},
+    };
+    # TODO: Add insert only mode to save a query
+    my $id = $self->get_or_insert($player_id, $to_insert);
+    return $id;
+}
+
+sub _has_card($self, $player_id, $booking_info, $colour) {
+    return (exists $booking_info->{$player_id}) &&
+           ($booking_info->{$player_id} eq $colour . "_CARD");
+}
+
+sub _insert_player_fixture($self, $info) {
+    my ($stmt, @bind) = $self->sqla->insert(
+        -into   => 'players_fixtures',
+        -values => $info,
+    );
+
+    return $self->dbh->do($stmt, undef, @bind);
 }
 
 =back
--- t/Mandyville/Players.t
+++ t/Mandyville/Players.t
@@ -1,11 +1,20 @@
 #!/usr/bin/env perl
 
-use Mojo::Base -strict;
+use Mojo::Base -strict, -signatures;
 
+use Mandyville::API::FootballData;
 use Mandyville::Countries;
+use Mandyville::Competitions;
 use Mandyville::Database;
+use Mandyville::Fixtures;
+use Mandyville::Teams;
+use Mandyville::Utils qw(find_file);
 
+use Mojo::File;
+use Mojo::JSON qw(decode_json);
+use SQL::Abstract::More;
 use Test::Exception;
+use Test::MockObject::Extends;
 use Test::More;
 
 ######
@@ -30,10 +39,11 @@ use Mandyville::Players;
     });
 
     my $players = Mandyville::Players->new({
+        api       => Mandyville::API::FootballData->new,
         countries => $countries,
         dbh       => $dbh->rw_db_handle(),
         sqla      => $sqla,
-    }); 
+    });
 
     dies_ok { $players->get_or_insert() } 'get_or_insert: dies with args';
 
@@ -57,5 +67,155 @@ use Mandyville::Players;
             'get_or_insert: correctly fetches player from db' );
 }
 
+######
+# TEST get_by_football_data_id
+######
+
+{
+    my $dbh  = Mandyville::Database->new;
+    my $sqla = SQL::Abstract::More->new;
+
+    my $countries = Mandyville::Countries->new({
+        dbh => $dbh->rw_db_handle(),
+    });
+
+    my $players = Mandyville::Players->new({
+        api       => Mandyville::API::FootballData->new,
+        countries => $countries,
+        dbh       => $dbh->rw_db_handle(),
+        sqla      => $sqla,
+    });
+
+    dies_ok { $players->get_by_football_data_id() }
+              'get_by_football_data_id: dies without args';
+
+    my $f_d_id = 10;
+
+    my $id = $players->get_by_football_data_id($f_d_id);
+
+    ok( !$id, 'get_by_football_data_id: returns undef for unknown player' );
+
+    my $player_info = {
+        first_name   => 'Cristiano Ronaldo',
+        last_name    => '',
+        country_name => 'Portugal',
+    };
+
+    $players->get_or_insert($f_d_id, $player_info);
+
+    $id = $players->get_by_football_data_id($f_d_id);
+
+    ok( $id, 'get_by_football_data_id: returns ID for valid player' );
+}
+
+######
+# TEST update_fixture_info
+######
+
+{
+    my $dbh  = Mandyville::Database->new;
+    my $sqla = SQL::Abstract::More->new;
+
+    my $teams = Mandyville::Teams->new({
+        dbh  => $dbh->rw_db_handle(),
+        sqla => $sqla,
+    });
+
+    my $countries = Mandyville::Countries->new({
+        dbh => $dbh->rw_db_handle(),
+    });
+
+    my $comps = Mandyville::Competitions->new({
+        api       => Mandyville::API::FootballData->new,
+        countries => $countries,
+        dbh       => $dbh->rw_db_handle(),
+        sqla      => $sqla,
+    });
+
+    my $fixtures = Mandyville::Fixtures->new({
+        comps => $comps,
+        dbh   => $dbh->rw_db_handle(),
+        sqla  => $sqla,
+        teams => $teams,
+    });
+
+    my $mock_api = Test::MockObject::Extends->new(
+        'Mandyville::API::FootballData'
+    );
+
+    $mock_api->mock( 'player', sub {
+        my ($self, $id) = @_;
+        _mock_player_api($id)
+    } );
+
+    my $players = Mandyville::Players->new({
+        api       => $mock_api,
+        comps     => $comps,
+        countries => $countries,
+        fixtures  => $fixtures,
+        dbh       => $dbh->rw_db_handle(),
+        sqla      => $sqla,
+    });
+
+    dies_ok { $players->update_fixture_info }
+              'update_fixture_info: dies without args';
+
+    my $fixture_info = _load_test_json('players.json');
+
+    throws_ok { $players->update_fixture_info($fixture_info) }
+                qr/Unknown competition/,
+                'update_fixture_info: dies on unknown competition';
+
+    $comps->get_or_insert('Europe', 250, 2001, 1);
+
+    ok( $players->update_fixture_info($fixture_info),
+        'update_fixture_info: updates successfully' );
+
+    my ($count) = $dbh->rw_db_handle()->selectrow_array(
+        'SELECT COUNT(1) FROM players_fixtures'
+    );
+
+    # Match the number of players in the test JSON
+    cmp_ok( $count, '==', 4,
+            'update_fixture_info: all player fixtures added' );
+}
+
 done_testing();
+
+sub _mock_player_api($id) {
+    if ($id == 3194) {
+        return {
+            firstName   => 'Dani',
+            lastName    => 'Carvajal',
+            nationality => 'Spain',
+        };
+    } elsif ($id == 50) {
+        return {
+            firstName   => 'Nacho',
+            lastName    => undef,
+            nationality => 'Spain',
+        };
+    } elsif ($id == 3754) {
+        return {
+            firstName   => 'Mohamed',
+            lastName    => 'Salah',
+            nationality => 'Egypt',
+        };
+    } elsif ($id == 3318) {
+        return {
+            firstName   => 'Adam',
+            lastName    => 'Lallana',
+            nationality => 'England',
+        };
+    } else {
+        die "Unknown player ID $id";
+    }
+}
+
+# TODO: Move into utils or similar
+sub _load_test_json($filename) {
+    my $full_path = find_file("t/data/$filename");
+    my $json = Mojo::File->new($full_path)->slurp;
+    return decode_json($json);
+}
 
